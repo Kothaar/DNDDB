188670f96ee0526db4572294c214e4ea7e986595
kburton501 <C$4p0gFIP!bd>
2021-06-18 15:21:38 -0700

    Pulls weapons stats based on ID from database.

--- src/dev/kellyburton/Item.java
+++ src/dev/kellyburton/Item.java
@@ -1,20 +1,49 @@
 package dev.kellyburton;
 
+import java.sql.*;
+
 public class Item {
     private int id;
     private String name;
     private int cost;
     private int weight;
 
-    Item(int id, String name, int cost, int weight){
+    Item(int id, String name, int cost, int weight) {
         this.id = id;
         this.name = name;
         this.cost = cost;
         this.weight = weight;
 
     }
+
+    Item(int id){
+        this.id = id;
+        try {
+            db_get();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    private void db_get() throws SQLException {
+        String jdbcURL = "jdbc:postgresql://localhost:8877/DND";
+        String username = "postgres";
+        String password = "test";
+        Connection connection = DriverManager.getConnection(jdbcURL, username, password);
+        Statement statement = connection.createStatement();
+        System.out.println("\nConnected to PostgreSQL");
+        String query1 = String.format("SELECT id, name, cost, weight FROM public.weapon WHERE id = %d;", this.id);
+        ResultSet result = statement.executeQuery(query1);
+        while(result.next()) {
+            this.name = result.getString("name");
+            this.cost = result.getInt("cost");
+            this.weight = result.getInt("weight");
+        }
+        connection.close();
+    }
+
     public void display(){
         System.out.printf("%d %s %d %d", this.id, this.name, this.cost, this.weight);
-
     }
+
 }
--- src/dev/kellyburton/Weapon.java
+++ src/dev/kellyburton/Weapon.java
@@ -1,27 +1,50 @@
 package dev.kellyburton;
+import java.sql.*;
 import java.util.Random;
 
 public class Weapon extends Item{
     private int num_dice;
     private int dice_type;
-    private String []properties;
+    private String damage_type;
 
 
-    Weapon(int id, String name, int cost, int weight, int num_dice, int dice_type, String[] properties) {
+    Weapon(int id, String name, int cost, int weight, int num_dice, int dice_type, String damage_type) {
         super(id, name, cost, weight);
         this.num_dice = num_dice;
         this.dice_type = dice_type;
-        this.properties = properties;
+        this.damage_type = damage_type;
+    }
+
+    Weapon(int id) throws SQLException {
+        super(id);
+        db_get(id);
+    }
+
+    private void db_get(int id) throws SQLException {
+        String jdbcURL = "jdbc:postgresql://localhost:8877/DND";
+        String username = "postgres";
+        String password = "test";
+        Connection connection = DriverManager.getConnection(jdbcURL, username, password);
+        Statement statement = connection.createStatement();
+        System.out.println("Connected to PostgreSQL");
+        String query1 = String.format(  "SELECT number_dice, dice_type, damage_type " +
+                                        "FROM public.weapon " +
+                                        "WHERE id = %d;", id);
+        ResultSet result = statement.executeQuery(query1);
+        while(result.next()) {
+            this.num_dice = result.getInt("number_dice");
+            this.dice_type = result.getInt("dice_type");
+            this.damage_type = result.getString("damage_type");
+        }
+        connection.close();
     }
 
     @Override
     public void display() {
         super.display();
-        System.out.printf(" %dD%d", this.num_dice, this.dice_type);
-        for (String str: this.properties){
-            System.out.printf(" %s", str);
-        }
+        System.out.printf(" %dD%d %s", this.num_dice, this.dice_type, this.damage_type);
     }
+
 
     public int damage() {
         Random r = new Random();
